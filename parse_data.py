53184ea41a466841a0793a22dc13cadc8371b9ad
Adithyan KV <adithyankv007@gmail.com>
2021-05-23 22:32:03 +0530

    Info on restaurants and items
     -The number of orders of each item and restaurants is being
      extracted

--- parse_data.py
+++ parse_data.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def main():
@@ -22,7 +23,22 @@ def main():
         median_order_value = np.median(price_list)
         std_order_value = np.std(price_list)
 
-        plot_price_distribution(price_list)
+        # plot_price_distribution(price_list)
+
+        # getting restaurant and item statistics
+        restaurant_freq, item_freq = get_item_and_restaurant_data(order_data)
+        # sorting by most frequented
+        restaurant_freq = restaurant_freq.sort_values(
+            by='Frequency', ascending=False)
+        item_freq = item_freq.sort_values(by='Frequency', ascending=False)
+
+        # number of restaurants and items tried
+        rest_tried = restaurant_freq.shape[0]
+        items_tried = item_freq.shape[0]
+
+        print(rest_tried, items_tried)
+        print(restaurant_freq.head(15))
+        print(item_freq.head(15))
 
         print(average_order_value, median_order_value, std_order_value)
 
@@ -59,6 +75,47 @@ def plot_price_distribution(price_list):
     plt.xlabel('Order value (₹)')
     plt.ylabel('Number of orders')
     plt.show()
+
+
+def get_item_and_restaurant_data(order_data):
+    restaurant_freq = dict()
+    item_freq = dict()
+    # iterate over all orders
+    for order in order_data:
+        # get and item data
+        restaurant = order['restaurant_name']
+        item_list = order['order_items']
+
+        # get number of times ordered
+        if restaurant not in restaurant_freq:
+            restaurant_freq[restaurant] = 1
+        else:
+            restaurant_freq[restaurant] += 1
+
+        # get item details and quantity
+        for item in item_list:
+            item_name = item['name']
+            quantity = int(item['quantity'])
+
+            if item_name not in item_freq:
+                item_freq[item_name] = quantity
+            else:
+                item_freq[item_name] += quantity
+
+    # converting to pandas dataframe for ease of manipulation
+    rest_list = [(rest, freq) for rest, freq in restaurant_freq.items()]
+    rest_names = [entry[0] for entry in rest_list]
+    rest_freq = [entry[1] for entry in rest_list]
+    restaurant_data = {'Restaurant': rest_names, 'Frequency': rest_freq}
+    restaurant_dataframe = pd.DataFrame.from_dict(restaurant_data)
+
+    item_list = [(item, freq) for item, freq in item_freq.items()]
+    item_names = [entry[0] for entry in item_list]
+    item_freq = [entry[1] for entry in item_list]
+    item_data = {'Item': item_names, 'Frequency': item_freq}
+    item_dataframe = pd.DataFrame.from_dict(item_data)
+
+    return restaurant_dataframe, item_dataframe
 
 
 def print_all_orders(order_data):
