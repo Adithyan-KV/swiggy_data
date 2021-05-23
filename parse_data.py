14eed795c95dcaebf38bfed2ac4e12b17c99596f
Adithyan KV <adithyankv007@gmail.com>
2021-05-23 19:32:46 +0530

    Extracting items and date of order
     -Implemented extracting out details like date and time and also
      all items and their quantities

--- parse_data.py
+++ parse_data.py
@@ -7,12 +7,27 @@ def main():
         # load json data
         order_data = json.load(file)
 
-        # total number of orders
-        num_orders = len(order_data)
+        print_all_orders(order_data)
 
-        # iterate through data for each order
-        for index, order in enumerate(order_data):
-            print(index)
+
+def print_all_orders(order_data):
+    # iterate through data for each order
+    for index, order in enumerate(order_data):
+
+        order_id = order['order_id']
+
+        # date and time of order
+        date_time = order['order_time']
+        print(date_time)
+
+        # list containing all items in order
+        item_list = order['order_items']
+
+        for item in item_list:
+            item_name = item['name']
+            quantity = item['quantity']
+            print(f'{item_name} x {quantity}')
+        print('')
 
 
 if __name__ == '__main__':
